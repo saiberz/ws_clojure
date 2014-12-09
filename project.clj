c4e3fcf46b718bb67aa34e3cf2b1e37e511dc8a7
Fredy Morales <saiberz@hotmail.com>
2014-12-09 00:10:11 -0500

    Namespaces changed

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(defproject no2day "0.1.0-SNAPSHOT"
+(defproject wsclj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.19"]
-                 [ring "1.2.1"]
                  [ring "1.3.2"]
-                 [cheshire "5.3.1"]
                  [compojure "1.3.0"]
-                 [ring-cors "0.1.4"]
-                 [clj-wamp "1.0.2"]]
-  :main no2day.core)
+                 [ring-cors "0.1.4"]]
+  :main ^:skip-aot wsclj.core
+  :profiles {:uberjar {:aot :all}})
--- src/wsclj/core.clj
+++ src/wsclj/core.clj
@@ -1,16 +1,13 @@
-(ns no2day.core
+(ns wsclj.core
   (:use [compojure.core :only [defroutes GET]]
-         ring.middleware.cors)
+        ring.middleware.cors)
   (:require [org.httpkit.server :as http-kit]
             [ring.middleware.reload :as reload]
             [ring.util.response :refer [redirect]]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [clojure.tools.logging :as log]
-            [clj-wamp.server :as wamp]))
-
-;(use 'alembic.still)
-;(load-project)
+            )
+  (:gen-class :main true))
 
 (defonce server (atom nil))
 
@@ -26,36 +23,26 @@
   (http-kit/with-channel req con
     (swap! clients assoc con {:name "guest"})
     (http-kit/on-receive con (fn [data]
-                               (println "Data received: " )
+                                        ;                               (println "Data received: " )
                                (doseq [client @clients]
                                  (http-kit/send! (key client) (str data) false))))
     (http-kit/on-close con (fn [status]
                              (doseq [client @clients]
                                (http-kit/send! (key client) (str "Server closed:" con) false))
                              (swap! clients dissoc con)
-                             (println con " disconnected. status: " status)))))
-
-(defn change-name [con name]
- "Change user name"
- (swap! clients assoc con {:name name}))
-
-(defn remove-user []
-  (println "Usser Disconnected"))
+                                        ;                             (println con " disconnected. status: " status)
+                             ))))
 
 (defroutes routes
   (GET "/ws" [] ws)
-  (GET "/" [] (redirect "/index.html"))
+  (GET "/chat" [] (redirect "/index.html"))
+  (GET "/" [] "index")
   (route/resources "/"))
 
 (def app (-> (handler/site routes)
             reload/wrap-reload))
 
 (defn -main [& args]
-  (stop-server)
- (reset! server (http-kit/run-server app {:port 8090 :join? false}))
+;  (stop-server)
+  (reset! server (http-kit/run-server app {:port 8080 :join? false}))
 )
-
-;(doseq [client @clients]
-;    (http-kit/send! (key client) "ripusca" false))
-
-(-main)
