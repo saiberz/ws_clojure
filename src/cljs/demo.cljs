3487d157584114ce0d0abf4188af190e56a1e6c2
Fredy M. <saiberz@hotmail.com>
2014-12-11 21:13:13 -0500

    CleanUp demo.cljs

--- src/cljs/demo.cljs
+++ src/cljs/demo.cljs
@@ -3,23 +3,6 @@
             [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
-
-(.log js/console "hi cljs")
-
-(defn stripe [text bgc]
-  (let [st #js {:backgroundColor bgc}]
-    (dom/li #js {:style st} text)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (om/root                                                      ;;
-;;  (fn [app owner]                                               ;;
-;;    (om/component                                              ;;
-;;     (apply dom/ul #js {:className "animals"}                  ;;
-;;            (map stripe (:list app) (cycle ["#ff0" "fff"]))))) ;;
-;;  app-state                                                    ;;
-;;  {:target (. js/document (getElementById "demo"))})           ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (def app-data
   (atom
    {:users
